512436d2ce517391c8cce567b9d9d6c67010a4ea
fbenaglia <fbenaglia@hotmail.com>
2025-01-02 04:08:32 -0300

    first commit

---
+++ src/main/java/com/fab/reactive_web_service/ReactiveWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fab.reactive_web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveWebServiceApplication.class, args);
+	}
+
+}
